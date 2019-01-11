c1ad56bc677e93c3a4940812cae96227ce9ecbca
Erick Efraín <vargas.erick030997@gmail.com>
2019-01-11 17:46:55 -0600

    Create 374.cpp

---
+++ Ejercicios/UVA/374.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int lli;
+
+lli power(lli b, lli e){
+    lli ans = 1;
+    while(e){
+        if(e & 1)
+            ans *= b;
+
+    }
+}
+
+
+int main(){
+    lli B, P, M;
+    
+    cin >> B >> P >> M;
+
+    cout << power();
+    return 0;
+}
