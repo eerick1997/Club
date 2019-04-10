9e9db4321eebc30bc6d590fd67ee7306e3357810
Erick Efraín <vargas.erick030997@gmail.com>
2019-04-10 12:20:41 -0500

    Update

---
+++ Contest/teams/first/d.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int distance(pair<int, int> start, pair<int, int> target){
+    return (int)( sqrt( pow(start.first - target.first, 2) + pow(start.second - target.second, 2) ) );
+}
+
+int main(){
+    int N;
+    int d;
+    pair<int, int> start, target;
+    cin >> N >> start.first >> start.second >> target.first >> target.second;
+    d = distance(start, target);
+    if(d == N){
+        cout << (d) << endl;
+    } else if(d > N) {
+        cout << 0 << endl;
+    } else {
+        cout << abs( N - (d / 2) ) << endl;
+    }
+    return 0;
+}
