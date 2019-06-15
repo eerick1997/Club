2025c934ec1b17a58d9c44f7bee22f810c4f5412
Erick <vargas.erick030997@gmail.com>
2019-06-15 15:04:47 -0500

    Update

--- Contest/Gran premio de México 2019/Segunda fecha/K.cpp
+++ Contest/Gran premio de México 2019/Segunda fecha/K.cpp
@@ -2,14 +2,15 @@
 
 using namespace std;
 typedef long long int lli;
-const lli MINF = -1 * (10e10);
+
+const lli MINFTY = -1000000;
 typedef vector< vector <lli> > Matrix;
 typedef vector< lli > vi;
 
 void printMatrix(Matrix &m){
     cout << endl;
-    for(int i = 0; i < m.size(); i++){
-        for(int j = 0; j < m[i].size(); j++){
+    for(int i = 1; i < m.size(); i++){
+        for(int j = 1; j < m[i].size(); j++){
             cout << m[i][j] << "\t";
         }
         cout << endl;
@@ -23,20 +24,33 @@ int main(){
     cin.tie(0);
 
     cin >> rows >> cols;
-    Matrix m( rows, vi(cols, 0) );
+    Matrix m( rows + 1, vi(cols + 1, 0) );
     vi k_r( rows, 0 );
-    for(lli i = 0; i < rows; i++){
-        for(lli j = 0; j < cols; j++){
+    for(lli i = 1; i <= rows; i++){
+        for(lli j = 1; j <= cols; j++){
             cin >> v;
-            m[i][j] = (v == 0) ? 1 : MINF;
+            m[i][j] = (v == 0) ? 1 : MINFTY;
         }
     }
 
+    printMatrix(m);
+    //Acummulative sum 2D
+    for(int i = 1; i <= rows; i++){
+        //Making the sum by rows
+        for(int j = 1; j <= cols; j++)
+            m[i][j] += m[i][j - 1];
+
+        //Making the sum by cols
+        for(int j = 1; j <= cols; j++)
+            m[i][j] += m[i - 1][j];
+    }
+
+    printMatrix(m);
     //2D kadane's algorithm
-    for(int l = 0, sum = 0; l < cols; l++){
-        for(int r = l, sum = 0; r < cols; r++){
+    for(int l = 1, sum = 0; l <= cols; l++){
+        for(int r = l, sum = 0; r <= cols; r++){
             area = 0;
-            for(int row = 0; row < r; row++){
+            for(int row = 1; row < r; row++){
                 if(l > 0)
                     area += m[row][r] - m[row][l - 1];
                 else 
