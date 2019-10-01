ecb0f2698d03a25e622e2880f8f61af3e80e3650
Erick <vargas.erick030997@gmail.com>
2019-10-01 12:45:49 -0500

    Update

--- Contest/Codeforces div 2/29 sep 2019/b.cpp
+++ Contest/Codeforces div 2/29 sep 2019/b.cpp
@@ -10,71 +10,54 @@ const lli emptyCell = 0;
 const lli fillCell = 1;
 const lli none = -1;
 
-lli C[ 1005 ][ 1005 ];
-
-void Combinatorics( lli n, lli k ){
-    
-    for( lli i = 0; i <= n; i++ ){
-        for( lli j = 0; j <= min(i, k); j++ ){
-            if( j == 0 || j == i )
-                C[ i ][ j ] = 1;
-            else 
-                C[ i ][ j ] = (C[ i - 1 ][ j - 1 ] + C[ i - 1 ][ j ]) % MOD;
-        }
-    }
-}
-
 int main(){
+    ios::sync_with_stdio( false );
+    cout.tie( nullptr );
+    cin.tie( nullptr );
     lli height, width;
     vector< vii > matrix;    
     cin >> height >> width;
     matrix.assign( height + 2, vii( width + 2, none ));
 
-    Combinatorics( 1001, 1001 );
-
+    //Columns
     for( int i = 1, v; i <= height; i++ ){
         cin >> v;
-        if( v > 0 ){
-            for( int j = 1; j <= v; j++ ){
-                matrix[ i ][ j ] = fillCell;
-            }
-            matrix[ i ][ v + 1 ] = emptyCell;
-        } else 
-            matrix[ i ][ 1 ] = emptyCell;
+        for( int j = 1; j <= v; j++ ){
+            matrix[ i ][ j ] = fillCell;
+        }
+        matrix[ i ][ v + 1 ] = emptyCell; 
     }
 
+    //row
     for( int i = 1, v; i <= width; i++ ){
         cin >> v;
-        if( v > 0 ){
-            for( int j = 1; j <= v; j++ ){
-                if( matrix[ j ][ i ] == fillCell || matrix[ j ][ i ] == none )
-                    matrix[ j ][ i ] = fillCell;
-                else{
-                    cout << 0 << endl;
-                    return 0;
-                } 
-
+        for( int j = 1; j <= v; j++ ){
+            lli &cell = matrix[ j ][ i ];
+            if( cell == fillCell || cell == none )
+                cell = fillCell;
+            else{
+                cout << 0 << endl;
+                return 0;
             }
-            matrix[ v + 1 ][ i ] = emptyCell;
-        } else 
-            matrix[ 1 ][ i ] = emptyCell;
+        }
+        lli &cell = matrix[ v + 1 ][ i ];
+        if( cell == emptyCell || cell == none )
+            cell = emptyCell;
+        else {
+            cout << 0 << endl;
+            return 0;
+        }
     }
 
     lli sumEmptyCells = 0;
+    lli ans = 1;
     for( int i = 1; i <= height; i++ ){
         for( int j = 1; j <= width; j++ ){
             if( matrix[ i ][ j ] == none )
-                sumEmptyCells++;
-            cout << matrix[ i ][ j ] << "\t";
+                ans = ( (ans * 2) % MOD );
         }
-        cout << endl;
     }
 
-    lli ans = 0;
-    cout << endl << sumEmptyCells << endl;
-    for( int i = 0; i <= sumEmptyCells - 30; i++ ){
-        ans += ( C[ sumEmptyCells ][ i ] % MOD);
-    }
 
     cout << ans << endl;
 
