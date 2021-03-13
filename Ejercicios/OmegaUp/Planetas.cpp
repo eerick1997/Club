6b9d4d86471c798b6e92e9a3504d5f2afbe6439e
Erick Vargas <vargas.erick030997@gmail.com>
2021-03-12 22:07:30 -0600

    Gitignore
    Added some files to gitignore

--- Ejercicios/OmegaUp/Planetas.cpp
+++ Ejercicios/OmegaUp/Planetas.cpp
@@ -1,43 +1,47 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-vector< double > planets;
-int n;
-//\sum^{ n }_{ 0 }{ \frac{ 1 }{ X_{i} - M } }
-double SumOfForces( double middle ){
-    double sum = 0.0;
-    for( int i = 0; i < n; i++ ){
-        sum += 1 / ( planets[ i ] - middle );
-    }
-    return sum;
+
+vector<int> planets;
+
+double force(double M){
+    double ans = 0;
+    for(int i = 0; i < planets.size(); i++)
+        ans += 1 / (planets[i] - M);
+    return ans;
 }
 
 int main(){
-    ios::sync_with_stdio( false );
-    cout.tie( nullptr );
-    cin.tie( nullptr );
-    cin >> n;
-    planets.resize( n );
-    for( int i = 0; i < n; i++)
-        cin >> planets[ i ];
-    sort( planets.begin(), planets.end() );
-    cout << n - 1 << endl; 
-    for( int i = 0; i < n - 1; i++ ){
-        double begin = planets[ i ];
-        double end = planets[ i + 1 ];
-        double middle;
-        for( int i = 0; i < 25; i++ ){
-            middle = ( begin + end ) / 2;
-            if( SumOfForces( middle ) < 0 ){
-                begin = middle;
-            } else {
-                end = middle;
-            }
-        }
-        cout << fixed;
-        cout << setprecision( 3 );
-        cout << middle << " "; 
+
+    int N;
+    cin >> N;
+    while(N--){
+        double p;
+        cin >> p;
+        planets.push_back(p);
     }
-    cout << endl;
+
+    sort(planets.begin(), planets.end());
+
+    cout << (planets.size() - 1) << endl;
+    for(int i = 1; i < planets.size(); i++){
+
+        double begin = planets[i - 1];
+        double end = planets[i];
+
+        for(int j = 0; j < 25; j++){
+
+            double M = (begin + end) / 2;
+            double ans = force(M);
+            if(ans > 0)
+                end = M;
+            else
+                begin = M;
+        }
+        std::cout << std::fixed;
+        std::cout << std::setprecision(3);
+        cout << begin << " ";
+    }
+
     return 0;
 }
